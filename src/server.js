f70b8646851792f6d9b2b88defbac4fd1e5c6856
trungduc512 <doductrung234@gmail.com>
2025-11-20 22:34:35 +0700

    Implement Redis caching for document retrieval and initialize Redis connection

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import { socketCtrl } from './controllers/socket.controller.js';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import userRouter from './routes/user.routes.js';
-import documentRouter from './routes/document.routes.js';
-import dbConnect from './utils/dbConnect.js';
-import cors from 'cors';
+import express from "express";
+import { Server } from "socket.io";
+import { socketCtrl } from "./controllers/socket.controller.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import userRouter from "./routes/user.routes.js";
+import documentRouter from "./routes/document.routes.js";
+import dbConnect from "./utils/dbConnect.js";
+import cors from "cors";
+import { initRedis } from "./utils/redisConnect.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,25 +18,28 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/documents', documentRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/documents", documentRouter);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 const server = app.listen(PORT, async () => {
   await dbConnect();
+  console.log("Database connected successfully");
+  await initRedis();
+  console.log("Redis initialized successfully");
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
